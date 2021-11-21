97e64e4469e50d34af633a482b0d77337d8a5d53
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-11-21 19:59:17 -0300

    feat: demo 02 serverlessframework

---
+++ demo02-sls-framework/run.sh
@@ -0,0 +1,20 @@
+# instalar
+npm i -g serverless
+
+# sls inicializar 
+sls 
+
+# sempre fazer deploy antes de tudo para verificar se está com ambiente ok
+sls deploy
+
+# invocar na AWS
+sls invoke -f hello
+
+# invocar local
+sls invoke local -f hello -l
+
+# logs
+sls logs -f hello -t
+
+# remove
+sls remove

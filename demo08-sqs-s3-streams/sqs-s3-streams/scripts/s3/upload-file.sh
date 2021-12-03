f0d4769fdab4d70b538ab3bf8409cf1cbed39d2e
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2021-12-03 11:22:53 -0300

    feature: final config infra demo08-sqs-s3-streams

--- demo08-sqs-s3-streams/sqs-s3-streams/scripts/s3/upload-file.sh
+++ demo08-sqs-s3-streams/sqs-s3-streams/scripts/s3/upload-file.sh
@@ -3,7 +3,9 @@
 
 
 aws \
-  --endpoint-url=http://localhost:4566 s3 cp $FILE_PATH s3://$BUCKETNAME
+s3 cp $FILE_PATH s3://$BUCKETNAME \
+  --endpoint-url=http://localhost:4566 
   
 aws \
-  --endpoint-url=http://localhost:4566 s3 ls s3://$BUCKETNAME
+s3 ls s3://$BUCKETNAME \
+  --endpoint-url=http://localhost:4566 
--- demo08-sqs-s3-streams/sqs-s3-streams/src/s3.listener.js
+++ demo08-sqs-s3-streams/sqs-s3-streams/src/s3.listener.js
@@ -1,4 +1,40 @@
+const AWS = require('aws-sdk');
 class Handler {
+  constructor({s3Svc, sqsSvc}){
+    this.s3Svc = s3Svc
+    this.sqsSvc = sqsSvc
+  }
+  static getSdks(){
+     const host = process.env.LOCALSTACK_HOST || "localhost"
+     const s3port = process.env.S3_PORT || '4572'
+     const sqsPort = process.env.SQS_PORT || '4576'
+     const isLocal = process.env.IS_LOCAL
+     const s3endpoint = new AWS.Endpoint(
+       `htpp:${host}:${s3port}`
+     )
+     const s3config = {
+       endpoint: s3endpoint,
+       s3ForcePathStyle: true
+     }
+
+     const sqsEndpoint = new AWS.Endpoint(
+       `http://${host}:${sqsPort}`
+     )
+
+     const sqsconfig = {
+       endpoint: sqsEndpoint,
+     }
+
+     if(!isLocal){
+       delete s3config.endpoint
+       delete sqsconfig.endpoint
+     }
+
+     return{
+       s3: new AWS.S3(s3config),
+       sqs: new AWS.SQS(sqsconfig)
+     }
+  }
   async main(event){
     try {
       return {
@@ -16,6 +52,9 @@ class Handler {
     }
   }
 }
-
-const handler = new Handler()
+const {s3, sqs} = Handler.getSdks
+const handler = new Handler({
+  sqsSvc: sqs,
+  s3Svc: s3
+})
 module.exports = handler.main.bind(handler)
